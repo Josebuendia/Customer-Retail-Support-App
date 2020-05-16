e61bbbe8ff7f2d1fe2f6e469c705ad08b6a7a264
Josebuendia <ubuntu@ip-172-31-38-139.ec2.internal>
2020-05-16 20:48:35 +0000

    Removed tables in DB.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_16_141721) do
+ActiveRecord::Schema.define(version: 2020_05_16_204509) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
